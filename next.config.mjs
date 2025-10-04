82d9ba6c1469081e750d7ad58eadbf8356d91e2b
TomasLonnblad <tomaslonnblad@gmail.com>
2025-10-04 12:19:54 +0200

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  cacheHandler:
+    process.env.NODE_ENV === "production"
+      ? "./custom-incremental-cache-handler.mjs"
+      : undefined,
+  cacheMaxMemorySize: 0,
+  experimental: {
+    taint: true
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
+  images: {
+    remotePatterns: [
 
-
+    ]
+  }
+};
 
 export default nextConfig;
